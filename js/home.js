2bac2c820b0a127e8061fc0f6eba1c9e7052547e
Michael Onyeabo <michyking1@gmail.com>
2023-03-09 00:20:04 +0100

    first commit

---
+++ js/home.js
@@ -0,0 +1,9 @@
+// The Nav bar?
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("nav");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
